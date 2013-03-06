e9aadd72f27af1ac0bc32c6e36a11bf6369835aa
Christopher Groskopf <staringmonkey@gmail.com>
2013-03-06 13:39:14 -0600

    Sort of works.

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,50 @@
 $(function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4)
-    });
+    var $window = $(window);
+    var $card_section = $('#cards');
+    var $cards = $('.card');
+    var $top_spacer = $('#top-spacer');
+    var $bottom_spacer = $('#bottom-spacer');
 
-    var html = JST.example(context);
+    var card_y_offset = $card_section.offset()[1];
+    var card_count = $cards.length;
+    var active_card = 0;
+    var $active_card = $($cards[0]);
+    var $next_card = $($cards[1]);
 
-    $('#template-example').html(html);
+    $active_card.show();
+    $next_card.show();
+
+    var card_height = $active_card.height();
+    $card_section.height(card_height);
+
+    var cards_height = card_height * card_count;
+    $bottom_spacer.height(cards_height);
+
+    function card_section_scrolled(ev) {
+        var y = $card_section.scrollTop();
+
+        new_active_card = Math.floor(y / card_height);
+
+        if (active_card != new_active_card) {
+            $active_card.hide();
+            $next_card.hide();
+            active_card = new_active_card;
+        
+            $next_card.css({ 'margin-top': '0px' });
+
+            $active_card = $($cards[active_card]);
+            $next_card = $($cards[active_card + 1]);
+
+            $active_card.show();
+            $next_card.show();
+        }
+
+        var offset = y - (y % card_height);
+
+        $top_spacer.height(offset);
+        $next_card.css({ 'margin-top': (-card_height + (y % card_height)) + 'px' });
+        $bottom_spacer.height(cards_height - offset);
+    }
+
+    $card_section.scroll(card_section_scrolled);
 });
