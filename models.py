6ca609bcb98059ea7df23e21f3fa3ad19e698618
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-03-06 15:13:12 -0500

    Format date string.

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
+import datetime
+
 from peewee import *
 
 db = SqliteDatabase('data/grain.db')
+
 
 class Incident(Model):
     inspection_no = PrimaryKeyField()
     name = CharField()
     age = IntegerField()
-    incident_date = DateField()
+    incident_date = CharField()
     state = CharField()
     zip_code = IntegerField()
     initial_fine = IntegerField()
@@ -16,3 +19,11 @@ class Incident(Model):
     class Meta:
         database = db
         db_table = 'grain'
+
+    def clean_incident_date(self):
+
+        date = datetime.date(
+            int(self.incident_date.split('-')[0].strip()),
+            int(self.incident_date.split('-')[1].strip()),
+            int(self.incident_date.split('-')[2].strip()))
+        return date.strftime('%b. %e, %Y')
