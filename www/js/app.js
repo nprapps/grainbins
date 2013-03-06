c22106bf2e1d99956cd843c9ca52c811b5f7bc88
Alyson Hurt <ahurt@npr.org>
2013-03-06 16:18:56 -0500

    add bars behind fine amounts

--- www/js/app.js
+++ www/js/app.js
@@ -7,4 +7,16 @@ $(function() {
     var html = JST.example(context);
 
     $('#template-example').html(html);
+    
+    
+    // size the bars
+    $bars = $('#cards').find('.bar');
+    $($bars).each(function() {
+    	var max_amt = 1600000;
+    	var this_amt = parseInt($(this).attr('data-amt'));
+    	if (!isNaN(this_amt)) {
+    		var this_width = (this_amt/max_amt) * 100;
+    		$(this).width(this_width + '%');
+    	}
+    });
 });
