df537aaea7618704abc734f6130a8e67212749db
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-03-07 10:36:17 -0500

    Updates models.py to take advantage of new Stiles fields in the Google doc.

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class Incident(Model):
     Inspection_no is _sorta_ a primary key.
     But some of them have dots and stuff in them.
     """
+    # First round of fields.
     inspection_no = PrimaryKeyField()
     name = CharField()
     age = IntegerField()
@@ -21,16 +22,30 @@ class Incident(Model):
     current_fine = IntegerField()
     narrative = TextField()
 
+    # Second round of fields.
+    postofficename = CharField()
+    lat = CharField()
+    long = CharField()
+    sqmiles = FloatField()
+    pop2010 = CharField()
+    cityfips = CharField()
+
     class Meta:
         database = db
         db_table = 'grain'
+
+    def osha_url(self):
+        """
+        Return the OSHA URL for this incident.
+        """
+        return
 
     def clean_incident_date(self):
         """
         Return a prettified version of the incident date.
         """
         date = datetime.date(
-            int(self.incident_date.split('-')[0].strip()),
-            int(self.incident_date.split('-')[1].strip()),
-            int(self.incident_date.split('-')[2].strip()))
+            int(self.incident_date[0:4].strip()),
+            int(self.incident_date[4:6].strip()),
+            int(self.incident_date[6:8].strip()))
         return date.strftime('%B %e, %Y')
