2c621208f24e0e2f07f80dfb001169404df7641b
Alyson Hurt <ahurt@npr.org>
2013-03-07 11:35:23 -0500

    style tweaks to state filters. extra fun on mobile

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,12 @@
 $(function() {
-    var $states = $('.filter-state'); 
     var $cards = $('.card');
+    var $state_hdr = $('.state-filters').find('h4');
+    var $state_btns = $('#filter-state-buttons');
+    var $states = $('.filter-state');
+
+    var max_bar_amt = 1600000; // per @stiles
     
+    // state filters
     $states.click(function() {
         var state = $(this).data('state');
 
@@ -15,14 +20,19 @@ $(function() {
         $states.removeClass('active');
         $(this).addClass('active');
     });
+    
+    // css classes to show/hide only defined for mobile view
+    $state_hdr.click(function() {
+    	$(this).toggleClass('active');
+    	$state_btns.toggleClass('active');
+    });
 
     // size the bars
     $bars = $('#cards').find('.bar');
     $($bars).each(function() {
-    	var max_amt = 1600000;
     	var this_amt = parseInt($(this).attr('data-amt'));
     	if (!isNaN(this_amt)) {
-    		var this_width = (this_amt/max_amt) * 100;
+    		var this_width = (this_amt/max_bar_amt) * 100;
     		$(this).width(this_width + '%');
     	}
     });
